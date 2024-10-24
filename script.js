76826f41f9b372cc5832f2207e92ac466c44cf97
adodoCoder <alavropulido@whccd.edu>
2024-10-23 19:03:16 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// this is a comment line
+/*
+this
+is
+a
+multiline
+comment
+*/
+console.log("Hello World!");
+
+console.log(8 * null); //result: 0
+
+console.log("5" + 1); // result: 51
+
+console.log("5" - 1);
+
+console.log("five" * 2);
+
+console.log(false == 0);
+
+console.log(5 > 2 && 6 > 3);
+
+console.log(false == false ? "equal" : "not equal");
+
+console.log(true || false);
