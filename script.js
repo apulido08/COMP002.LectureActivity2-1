be20bfe01c22e7b01ba95deb2b90936a4a89b4b1
adodoCoder <alavropulido@whccd.edu>
2024-10-23 19:04:55 -0700

    added results to js.script

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ console.log(8 * null); //result: 0
 
 console.log("5" + 1); // result: 51
 
-console.log("5" - 1);
+console.log("5" - 1); // result: 4
 
-console.log("five" * 2);
+console.log("five" * 2); // result: NaN
 
-console.log(false == 0);
+console.log(false == 0); // true
 
-console.log(5 > 2 && 6 > 3);
+console.log(5 > 2 && 6 > 3); //true
 
-console.log(false == false ? "equal" : "not equal");
+console.log(false == false ? "equal" : "not equal"); //equal
 
-console.log(true || false);
+console.log(true || false); // true
